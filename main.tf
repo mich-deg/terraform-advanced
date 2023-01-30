621a13e439bf83b9c3d1c0c0acf0003fda63f99b
Michael <lemichael1215@gmail.com>
2023-01-30 22:42:04 +0300

    added security_rule inline with security_group

--- main.tf
+++ main.tf
@@ -13,16 +13,55 @@ provider "azurerm" {
   features {}
 }
 
+# Define the Azure Resource Group
 resource "azurerm_resource_group" "advanced" {
     name="ad-terraform-rg"
     location = "eastus"
-
 #  Flag Azure Resources as Managed by Terraform using 'source' Tag
     tags = {
       source = "terraform"
     }
 }
+# Create Network Security Group and Rule
+resource "azurerm_network_security_group" "advanced" {
+  name = "ad-terraform-nsg"
+  location = azurerm_resource_group.advanced.location
+  resource_group_name = azurerm_resource_group.advanced.name
 
+  security_rule {
+    name = "SSH"
+    priority = 1001
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "22"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name = "HTTP"
+    priority = 1002
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "80"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name = "ICMP"
+    priority = 1003
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Icmp"
+    source_port_range = "*"
+    destination_port_range = "*"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+}
 #Creates Virtual Network
 resource "azurerm_virtual_network" "advanced" {
   name = "ad-terraform-vnet"
@@ -30,94 +69,45 @@ resource "azurerm_virtual_network" "advanced" {
   location = azurerm_resource_group.advanced.location
   resource_group_name = azurerm_resource_group.advanced.name
 }
-
+# Defines a Subnet
 resource "azurerm_subnet" "advanced" {
   name = "ad-terraform-subnet"
   resource_group_name = azurerm_resource_group.advanced.name
   virtual_network_name = azurerm_virtual_network.advanced.name
   address_prefixes = ["10.0.1.0/24"]
 }
-
-resource "azurerm_network_security_group" "advanced" {
-  name = "ad-terraform-nsg"
-  location = azurerm_resource_group.advanced.location
-  resource_group_name = azurerm_resource_group.advanced.name
-}
-
-resource "azurerm_network_security_rule" "advanced_http" {
-  name = "ad-terraform-http"
-  priority = 100
-  direction = "Inbound"
-  access = "Allow"
-  protocol = "Tcp"
-  source_port_range = "*"
-  destination_port_range = "80"
-  source_address_prefix = "*"
-  destination_address_prefix = "*"
-  resource_group_name = azurerm_resource_group.advanced.name
-  network_security_group_name = azurerm_network_security_group.advanced.name
-}
-
-resource "azurerm_network_security_rule" "advanced_ssh" {
-  name = "ad-terraform-http"
-  priority = 101
-  direction = "Inbound"
-  access = "Allow"
-  protocol = "Tcp"
-  source_port_range = "*"
-  destination_port_range = "22"
-  source_address_prefix = "*"
-  destination_address_prefix = "*"
-  resource_group_name = azurerm_resource_group.advanced.name
-  network_security_group_name = azurerm_network_security_group.advanced.name
-}
-
-resource "azurerm_network_security_rule" "advanced_icmp" {
-  name = "ad-terraform-icmp"
-  priority = 102
-  direction = "Inbound"
-  access = "Allow"
-  protocol = "Icmp"
-  source_port_range = "*"
-  destination_port_range = "*"
-  source_address_prefix = "*"
-  destination_address_prefix = "*"
-  resource_group_name = azurerm_resource_group.advanced.name
-  network_security_group_name = azurerm_network_security_group.advanced.name
-}
-
-
-resource "azurerm_public_ip" "advanced_nginx" {
-  name = "nginx-public-ip"
+#Define public IP Address
+resource "azurerm_public_ip" "advanced" {
+  name = "ad-terraform-pIP"
   location = azurerm_resource_group.advanced.location
   resource_group_name = azurerm_resource_group.advanced.name
   allocation_method = "Static"
 }
 
-resource "azurerm_network_interface" "advanced_nginx" {
-  name = "ad-nginx-nic"
+#Define a network interface
+resource "azurerm_network_interface" "advanced" {
+  name = "ad-terraform-nic"
   location = azurerm_resource_group.advanced.location
   resource_group_name = azurerm_resource_group.advanced.name
 
   ip_configuration {
-    name = "ad-nginx-config"
+    name = "ad-terraform-config"
     subnet_id = azurerm_subnet.advanced.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.advanced_nginx.id
   }
 }
 
-resource "azurerm_virtual_machine" "advanced_nginx" {
-  name = "ad-nginx"
+resource "azurerm_virtual_machine" "advanced" {
+  name = "vm-nginx"
   location = azurerm_resource_group.advanced.location
   resource_group_name = azurerm_resource_group.advanced.name
-  network_interface_ids = [azurerm_network_interface.advanced_nginx.id]
+  network_interface_ids = [azurerm_network_interface.advanced.id]
   vm_size = "Standard_DS1_v2"
   
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
   }
 
@@ -136,6 +126,7 @@ resource "azurerm_virtual_machine" "advanced_nginx" {
   os_profile_linux_config {
     disable_password_authentication = false
   }
+  tags = {
+    environment = "staging"
+  }
 }
-
-
