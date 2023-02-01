242dbdf0c5949a3efb12b35cade62a556e47bff4
Michael <lemichael1215@gmail.com>
2023-02-01 22:19:09 +0300

    added output

--- main.tf
+++ main.tf
@@ -154,3 +154,6 @@ data "azurerm_public_ip" "ad-ip-data" {
   resource_group_name = azurerm_resource_group.advanced-rg.name
 }
 
+output "public_ip_address" {
+  value = "${azurerm_linux_virtual_machine.nginxVM.name}: ${data.azurerm_public_ip.ad-ip-data.ip_address}"
+}
