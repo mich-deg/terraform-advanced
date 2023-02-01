e524c758bb836ea965035345cfb9a95a9cdcd881
Michael <lemichael1215@gmail.com>
2023-02-01 20:09:50 +0300

    added ssh_key & standalone network security group and network security rules

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 ### PROVIDER
 terraform {
-    required_providers {
+  required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.40.0"
     }
   }
@@ -11,111 +11,142 @@ provider "azurerm" {
   # Configuration options
   features {}
 }
-
 # Define the Azure Resource Group
-resource "azurerm_resource_group" "advanced" {
-    name= var.resource_group_name
-    location = var.location
-#  Flagging Azure resources as managed by terraform using 'source' Tag
-    tags = {
-      source = "terraform"
-    }
+resource "azurerm_resource_group" "advanced-rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    source      = "terraform"
+    environment = "dev"
+  }
 }
-# Create Network Security Group and Rule
-resource "azurerm_network_security_group" "advanced" {
-  name = "advanced-nsg"
-  location = azurerm_resource_group.advanced.location
-  resource_group_name = azurerm_resource_group.advanced.name
 
-  security_rule {
-    name = "SSH"
-    priority = 1001
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "22"
-    source_address_prefix = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name = "HTTP"
-    priority = 1002
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "80"
-    source_address_prefix = "*"
-    destination_address_prefix = "*"
-  }
-  security_rule {
-    name = "ICMP"
-    priority = 1003
-    direction = "Inbound"
-    access = "Allow"
-    protocol = "Icmp"
-    source_port_range = "*"
-    destination_port_range = "*"
-    source_address_prefix = "*"
-    destination_address_prefix = "*"
-  }
-}
 #Creates Virtual Network
-resource "azurerm_virtual_network" "advanced" {
-  name = "advanced-vnet"
-  address_space = ["10.0.0.0/16"]
-  location = azurerm_resource_group.advanced.location
-  resource_group_name = azurerm_resource_group.advanced.name
+resource "azurerm_virtual_network" "advanced-vn" {
+  name                = "advanced-vnet"
+  resource_group_name = azurerm_resource_group.advanced-rg.name
+  location            = azurerm_resource_group.advanced-rg.location
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+
 }
 # Defines a Subnet
-resource "azurerm_subnet" "advanced" {
-  name = "advanced-subnet"
-  resource_group_name = azurerm_resource_group.advanced.name
-  virtual_network_name = azurerm_virtual_network.advanced.name
-  address_prefixes = ["10.0.1.0/24"]
+resource "azurerm_subnet" "advanced-sn" {
+  name                 = "advanced-subnet"
+  resource_group_name  = azurerm_resource_group.advanced-rg.name
+  virtual_network_name = azurerm_virtual_network.advanced-vn.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
+# Create Network Security Group and Rule
+resource "azurerm_network_security_group" "advanced-nsg" {
+  name                = "advanced-nsg"
+  resource_group_name = azurerm_resource_group.advanced-rg.name
+  location            = azurerm_resource_group.advanced-rg.location
+  tags = {
+    environment = "dev"
+  }
+}
+resource "azurerm_network_security_rule" "advanced-SSH-rule" {
+  name                        = "SSH"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.advanced-rg.name
+  network_security_group_name = azurerm_network_security_group.advanced-nsg.name
+}
+resource "azurerm_network_security_rule" "advanced-HTTP-rule" {
+  name                        = "HTTP"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.advanced-rg.name
+  network_security_group_name = azurerm_network_security_group.advanced-nsg.name
+}
+resource "azurerm_network_security_rule" "advanced-ICMP-rule" {
+  name                        = "ICMP"
+  priority                    = 1003
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Icmp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.advanced-rg.name
+  network_security_group_name = azurerm_network_security_group.advanced-nsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "advanced-sga" {
+  subnet_id                 = azurerm_subnet.advanced-sn.id
+  network_security_group_id = azurerm_network_security_group.advanced-nsg.id
+}
+
 #Define public IP Address
-resource "azurerm_public_ip" "advanced" {
-  name = "advanced-pIP"
-  location = azurerm_resource_group.advanced.location
-  resource_group_name = azurerm_resource_group.advanced.name
-  allocation_method = "Static"
+resource "azurerm_public_ip" "advanced-IP" {
+  name                = "advanced-IP"
+  resource_group_name = azurerm_resource_group.advanced-rg.name
+  location            = azurerm_resource_group.advanced-rg.location
+  allocation_method   = "Dynamic"
+  tags = {
+    environment = "dev"
+  }
 }
 #Define a network interface
-resource "azurerm_network_interface" "advanced" {
-  name = "advanced-nic"
-  location = azurerm_resource_group.advanced.location
-  resource_group_name = azurerm_resource_group.advanced.name
-
+resource "azurerm_network_interface" "advanced-nic" {
+  name                = "advanced-nic"
+  resource_group_name = azurerm_resource_group.advanced-rg.name
+  location            = azurerm_resource_group.advanced-rg.location
   ip_configuration {
-    name = "advanced-config"
-    subnet_id = azurerm_subnet.advanced.id
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.advanced-sn.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.advanced-IP.id
   }
+  tags = {
+    environment = "dev"
+  }
+
 }
 
 resource "azurerm_linux_virtual_machine" "nginxVM" {
-  name = "advanced-nginxVM"
-  resource_group_name = azurerm_resource_group.advanced.name
-  location = azurerm_resource_group.advanced.location
-  size = "Standard_DS1_v2"
-  admin_username = "adminuser"
-  admin_password = "P@ssw0rd12!"
-  network_interface_ids = [azurerm_network_interface.advanced.id,]
+  name                = "advanced-nginxVM"
+  resource_group_name = azurerm_resource_group.advanced-rg.name
+  location            = azurerm_resource_group.advanced-rg.location
+  size                = "Standard_DS1_v2"
+  admin_username      = "adminuser"
+  # admin_password        = "P@ssw0rd12!"
+  network_interface_ids = [azurerm_network_interface.advanced-nic.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/terraformkey.pub")
+  }
 
   os_disk {
-    caching = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "18.04-LTS"
-    version = "latest"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 
-  disable_password_authentication = false
+  # disable_password_authentication = false
 }
 
--- variables.tf
+++ variables.tf
@@ -1,11 +1,12 @@
 variable "resource_group_name" {
-    type = string
-    description = "Resource Group name"
-    default = "advanced-rg"
+  type        = string
+  description = "Resource Group name"
+  default     = "ad-rg"
 }
 
 variable "location" {
-    type = string
-    description = "The location for deployment"
-    default = "East US"
+  type        = string
+  description = "The location for deployment"
+  default     = "East Us"
 }
+
